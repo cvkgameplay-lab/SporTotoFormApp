02722e84afe5584708a02e74a2712061f97bf881
cvkgameplay-lab <cvkgameplay@gmail.com>
2026-03-20 16:27:20 +0300

    Refactor

--- SporTotoFormApp/Services/OptimizationOptions.cs
+++ SporTotoFormApp/Services/OptimizationOptions.cs
@@ -3,13 +3,13 @@ namespace SporTotoFormApp.Services
     public sealed class OptimizationOptions
     {
         public int DesiredCouponCount { get; init; }
-        public int InitialTopCandidateLimit { get; init; } = 350000;
-        public int DiversePrePoolLimit { get; init; } = 70000;
-        public int ApiBudgetMultiplier { get; init; } = 80;
-        public int ApiConcurrency { get; init; } = 4;
+        public int InitialTopCandidateLimit { get; init; } = 500000;
+        public int DiversePrePoolLimit { get; init; } = 120000;
+        public int ApiBudgetMultiplier { get; init; } = 120;
+        public int ApiConcurrency { get; init; } = 6;
         public int MinHammingDistance { get; init; } = 5;
         public int MinHammingDistanceFinal { get; init; } = 4;
-        public int MonteCarloScenarioCount { get; init; } = 6000;
+        public int MonteCarloScenarioCount { get; init; } = 15000;
 
         public int GetApiBudget()
         {
